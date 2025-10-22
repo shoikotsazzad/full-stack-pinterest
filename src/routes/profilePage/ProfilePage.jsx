6bdf782d5e6bae92882bc6bf5588451b7e33bd38
shoikotsazzad <mdshoikot202123@gmail.com>
2025-10-22 20:10:06 +0600

    Following and Followers Added

--- src/routes/profilePage/ProfilePage.jsx
+++ src/routes/profilePage/ProfilePage.jsx
@@ -6,6 +6,7 @@ import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router'
 import apiRequest from '../../utils/apiRequest'
 import Boards from '../../components/boards/Boards'
+import FollowButton from './FollowButton'
 
 const ProfilePage = () => {
 
@@ -33,12 +34,15 @@ const ProfilePage = () => {
       />
       <h1 className='profileName'>{data.displayName}</h1>
       <span className='profileUsername'>{data.username}</span>
-      <div className='followCounts'>10 followers . 20 followings</div>
+      <div className='followCounts'>{data.followerCount} followers . {data.followingCount} followings</div>
       <div className='profileInteractions'>
         <Image path="/general/share.svg" alt="" />
         <div className='profileButtons'>
           <button>Message</button>
-          <button>Follow</button>
+          <FollowButton 
+          isFollowing={data.isFollowing} 
+          username={data.username}
+          />
         </div>
         <Image path="/general/more.svg" alt="" />
       </div>
